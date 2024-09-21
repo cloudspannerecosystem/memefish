116f9ddb50e2b7274e9666e2847cc3181743afe9
apstndb <803393+apstndb@users.noreply.github.com>
2024-09-21 22:06:11 +0900

    Make SplitRawStatements result to struct

--- split.go
+++ split.go
@@ -2,14 +2,20 @@ package memefish
 
 import "github.com/cloudspannerecosystem/memefish/token"
 
+type RawStatement struct {
+	Pos, End  token.Pos
+	Statement string
+}
+
 // SplitRawStatements splits an input string to statement strings at terminating semicolons without parsing.
+// It preserves all comments.
 // Statements are terminated by `;`, `<eof>` or `;<eof>` and the minimum output will be []string{""}.
 // See [terminating semicolons].
 // This function won't panic but return error if lexer become error state.
-// filepath can be used in error message.
+// filepath can be empty, it is only used in error message.
 //
 // [terminating semicolons]: https://cloud.google.com/spanner/docs/reference/standard-sql/lexical#terminating_semicolons
-func SplitRawStatements(filepath, s string) ([]string, error) {
+func SplitRawStatements(filepath, s string) ([]*RawStatement, error) {
 	lex := &Lexer{
 		File: &token.File{
 			FilePath: filepath,
@@ -17,11 +23,14 @@ func SplitRawStatements(filepath, s string) ([]string, error) {
 		},
 	}
 
-	var result []string
+	var result []*RawStatement
 	var firstPos token.Pos
 	for {
 		if lex.Token.Kind == ";" {
-			result = append(result, s[firstPos:lex.Token.Pos])
+			result = append(result, &RawStatement{Pos: firstPos,
+				End:       lex.Token.Pos,
+				Statement: s[firstPos:lex.Token.Pos],
+			})
 			if err := lex.NextToken(); err != nil {
 				return nil, err
 			}
@@ -36,13 +45,18 @@ func SplitRawStatements(filepath, s string) ([]string, error) {
 
 		if lex.Token.Kind == token.TokenEOF {
 			if lex.Token.Pos != firstPos {
-				result = append(result, s[firstPos:lex.Token.Pos])
+				result = append(result, &RawStatement{Pos: firstPos,
+					End:       lex.Token.Pos,
+					Statement: s[firstPos:lex.Token.Pos],
+				})
 			}
 			break
 		}
 	}
 	if len(result) == 0 {
-		return []string{""}, nil
+		return []*RawStatement{
+			{Pos: token.Pos(0), End: token.Pos(0), Statement: ""},
+		}, nil
 	}
 	return result, nil
 }
