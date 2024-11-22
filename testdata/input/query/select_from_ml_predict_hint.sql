cdf97d98b940bac9e3243e5183d3786e2d460128
apstndb <803393+apstndb@users.noreply.github.com>
2024-11-23 08:34:27 +0900

    Fix indentation

--- testdata/input/query/select_from_ml_predict_hint.sql
+++ testdata/input/query/select_from_ml_predict_hint.sql
@@ -1,7 +1,7 @@
 -- https://cloud.google.com/spanner/docs/ml-tutorial-generative-ai?hl=en#register_a_generative_ai_model_in_a_schema
 SELECT content
 FROM ML.PREDICT(
-        MODEL TextBison,
-        (SELECT "Is 13 prime?" AS prompt),
-        STRUCT(256 AS maxOutputTokens, 0.2 AS temperature, 40 as topK, 0.95 AS topP)
-     ) @{remote_udf_max_rows_per_rpc=1}
+    MODEL TextBison,
+    (SELECT "Is 13 prime?" AS prompt),
+    STRUCT(256 AS maxOutputTokens, 0.2 AS temperature, 40 as topK, 0.95 AS topP)
+) @{remote_udf_max_rows_per_rpc=1}
