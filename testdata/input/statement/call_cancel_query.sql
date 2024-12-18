e2c2df835d4e8fe18a57a703740d07eaec064be6
apstndb <803393+apstndb@users.noreply.github.com>
2024-12-18 20:01:01 +0900

    Update testdata

---
+++ testdata/input/statement/call_cancel_query.sql
@@ -0,0 +1,1 @@
+CALL cancel_query("12345")
---
+++ testdata/input/statement/call_path.sql
@@ -0,0 +1,2 @@
+-- https://github.com/google/zetasql/blob/a516c6b26d183efc4f56293256bba92e243b7a61/zetasql/parser/testdata/call.test#L15C1-L15C26
+call schema.myprocedure()
