b654d49839f02f18448abc4ea19ab7434b9d9412
apstndb <803393+apstndb@users.noreply.github.com>
2025-01-11 05:12:15 +0900

    Fix AlterDatabase.end

--- ast/ast.go
+++ ast/ast.go
@@ -2235,7 +2235,7 @@ type CreateDatabase struct {
 //	ALTER DATABASE {{.Name | sql}} SET {{.Options | sql}}
 type AlterDatabase struct {
 	// pos = Alter
-	// end = Name.end
+	// end = Options.end
 
 	Alter token.Pos // position of "ALTER" keyword
 
