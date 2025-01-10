5280c98e82703ebc757e555c4ffd14fdd2d2114b
apstndb <803393+apstndb@users.noreply.github.com>
2025-01-11 05:11:48 +0900

    Fix Selector.end

--- ast/ast.go
+++ ast/ast.go
@@ -1361,7 +1361,7 @@ type BetweenExpr struct {
 //	{{.Expr | sql}}.{{.Ident | sql}}
 type SelectorExpr struct {
 	// pos = Expr.pos
-	// end = Ident.pos
+	// end = Ident.end
 
 	Expr  Expr
 	Ident *Ident
